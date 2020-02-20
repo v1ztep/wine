f685801d29d2eb17248fbcd03d020721d0d2f8bc
v1ztep <v1ztep@gmail.com>
2020-02-20 21:58:48 +0300

    Добавил подгрузку вин из Excel

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
+import pandas as pd
 
 
 WINERY_BASE_YEAR = datetime.datetime(year=1920, month=1, day=1, hour=0).year
 now_year = datetime.datetime.now().year
 delta = now_year - WINERY_BASE_YEAR
+
+excel_data_df = pd.read_excel('wine.xlsx', usecols=['Название','Сорт','Цена','Картинка'])
+wines_dict = excel_data_df.to_dict(orient='record')
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -17,6 +21,7 @@ template = env.get_template('template.html')
 
 rendered_page = template.render(
     winery_age=f"Уже {delta} лет с вами",
+    wines=wines_dict
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
