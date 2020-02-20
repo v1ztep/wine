b988ef151731d66786ef77c5e28ae42fd54000ec
v1ztep <v1ztep@gmail.com>
2020-02-20 18:42:10 +0300

    Добавил карточки и дельту года основания и настоящего

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
+
+
+WINERY_BASE_YEAR = datetime.datetime(year=1920, month=1, day=1, hour=0).year
+now_year = datetime.datetime.now().year
+delta = now_year - WINERY_BASE_YEAR
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    winery_age=f"Уже {delta} лет с вами",
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
