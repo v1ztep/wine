aab3b213d95b82139b8f6d0f5720943c00291372
v1ztep <v1ztep@gmail.com>
2020-02-27 18:47:01 +0300

    Вынес конфиг из кода

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas as pd
+import argparse
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 WINERY_BASE_YEAR = 1920
 now_year = datetime.datetime.now().year
 delta = now_year - WINERY_BASE_YEAR
 
-excel_data_df = pd.read_excel('wine.xlsx', usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'], keep_default_na=False)
+parser = argparse.ArgumentParser(description='Optional argument path to excel file')
+parser.add_argument(
+    '--path',
+    type=str,
+    default=os.getenv('DEFAULT_PATH_TO_EXCEL_FILE', 'wine.xlsx'),
+    help='provide an path to excel file'
+)
+path_to_excel = parser.parse_args()
+
+excel_data_df = pd.read_excel(path_to_excel.path,
+                              usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
+                              keep_default_na=False)
 unique_wine_categories = excel_data_df.Категория.unique()
 wines_info = excel_data_df.to_dict(orient='record')
-
 
 env = Environment(
     loader=FileSystemLoader('.'),
